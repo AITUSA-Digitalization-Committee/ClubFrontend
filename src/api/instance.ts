157f2760d0fd0d6802156512b0f1e39dfc9e3739
Yerassyl <108489760+yeunikey@users.noreply.github.com>
2025-05-11 00:52:10 +0500

    Update instance.ts

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -4,7 +4,7 @@ export const defaultOptions: Record<string, string> = {
   "Content-Type": "application/json",
 };
 
-export const baseUrl = "http://127.0.0.1:8000";
+export const baseUrl = "https://clubs-api.yeunikey.dev";
 
 export const api = xior.create({
   baseURL: baseUrl,
